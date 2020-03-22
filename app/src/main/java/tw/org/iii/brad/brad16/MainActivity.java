cd097dd02c9e0b41e337a9ea4e64f7920703326e
user <brad@brad.tw>
2020-03-22 10:40:41 +0800

    5

--- app/src/main/java/tw/org/iii/brad/brad16/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad/brad16/MainActivity.java
@@ -1,7 +1,12 @@
 package tw.org.iii.brad.brad16;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,11 +28,39 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
     private TextView mesg;
     private ImageView img;
+    private String[] permissions =
+            {Manifest.permission.WRITE_EXTERNAL_STORAGE,};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    123);
+        }else{
+            init();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            init();
+        }
+
+    }
+
+    private void init(){
         mesg = findViewById(R.id.mesg);
         img = findViewById(R.id.img);
     }
